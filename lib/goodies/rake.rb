5f542fe91b0f02387f505131fdfcf128563bd9f9
Kerry Wilson <kerry@allthingswilson.com>
2010-02-17 19:25:49 -0600

    Add rake file

---
+++ lib/goodies/rake.rb
@@ -0,0 +1,17 @@
+require 'rake/gempackagetask'
+
+class Rake::GemPackageTask
+
+  def define_intercepted
+    define_method_call
+    desc "Install the gem file #{gem_file}"
+    task :install => :gem do
+      require 'rubygems/installer'
+      Gem::Installer.new(File.join('pkg',gem_file)).install
+    end
+  end
+
+  alias define_method_call define
+  alias define define_intercepted
+
+end
