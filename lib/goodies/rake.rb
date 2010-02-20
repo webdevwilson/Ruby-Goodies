f5fb937436156252f201592685be7df66a1fe4ad
Kerry Wilson <kerry@allthingswilson.com>
2010-02-19 22:07:28 -0600

    Moved install task to gem namespace

--- lib/goodies/rake.rb
+++ lib/goodies/rake.rb
@@ -4,10 +4,12 @@ class Rake::GemPackageTask
 
   def define_intercepted
     define_method_call
-    desc "Install the gem file #{gem_file}"
-    task :install => :gem do
-      require 'rubygems/installer'
-      Gem::Installer.new(File.join('pkg',gem_file)).install
+    namespace 'gem' do
+      desc "Install the gem file #{gem_file}"
+      task :install => :gem do
+        require 'rubygems/installer'
+        Gem::Installer.new(File.join('pkg',gem_file)).install
+      end
     end
   end
 
